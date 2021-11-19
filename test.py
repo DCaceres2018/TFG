9df9f02b8db4bd9fde33d6981dbbbf2099245894
DCaceres2018 <d.caceres.2018@alumnos.urjc.es>
2021-11-19 15:31:41 +0100

    Añadido test básico de 1 archivo

--- test.py
+++ test.py
@@ -1,8 +1,53 @@
 import nltk
 from module import filtrador_métodos as f
-
+import os
+from xml.dom import minidom
 
 if __name__ == '__main__':
-    sentence= "Sample of non ASCII: Ceñía. feet How to bats remove stopwords and puntuactions?"
-    frase = f.pre_process(sentence)
-    f.lematizacion(frase)
+    ruta="./sciEntsBank"
+    """Para listar todos los xml
+    contenido=os.listdir(ruta)
+    for i in contenido:
+        cont=os.listdir(ruta+"/"+i)
+        for j in cont:
+            print(j)"""
+
+    doc = minidom.parse(ruta+"/test-unseen-answers/EM-inv1-45b.xml")
+    question=doc.getElementsByTagName("questionText")[0]
+    print(question.firstChild.data)
+    respuestaBuena = doc.getElementsByTagName("referenceAnswer")[0]
+    buena=respuestaBuena.firstChild.data
+    respuestasDadas= doc.getElementsByTagName("studentAnswer")
+    respuestas=[]
+    for i in range(len(respuestasDadas)):
+        respuestas.append(respuestasDadas[i].firstChild.data)
+    print(respuestas)
+
+    respuestas.append(buena)
+    sentence= f.pre_process(buena)
+    print(buena)
+    print(sentence)
+    sentenceBuenaLematizada = f.lematizacion(sentence)
+    print(sentenceBuenaLematizada)
+    respuestasPre=[]
+    respuestasLem=[]
+    for i in respuestas:
+        respuestasPre.append(f.pre_process(i))
+    for i in respuestasPre:
+        respuestasLem.append(f.lematizacion(i))
+
+
+
+    a=f.featureExtraction(respuestas)
+    print(a)
+    for i in a:
+        b = f.get_cosine_similarity(i, a[-1])
+        print(b)
+
+
+    corpus=["A girl is styling her hair.","A girl is styling her hair."]
+    list=f.featureExtraction(corpus)
+    print(list)
+    print()
+    d=f.get_sif_feature_vectors(corpus[0],corpus[1])
+    print(d)
